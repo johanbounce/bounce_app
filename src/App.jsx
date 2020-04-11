02b0a0a32d621980f87b3effed2e9d0123b49774
bounce <johan@waytogo.se>
2020-04-11 18:41:32 +0200

    added header to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Greeting from "./Greeting";
 import { Grommet, Main } from "grommet";
 import { grommet } from "grommet/themes";
 import TopBox from "./TopBox";
+import Header from "./Header"
 
 const App = () => {
   return (
     <>
       <Grommet full theme={grommet}>
+        <Header />
         <Main fill align="center">
           <Greeting />
           <TopBox />
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <Header background="C2E189">
+
+    </Header>
   )
 }
 
